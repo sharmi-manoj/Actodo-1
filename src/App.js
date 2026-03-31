ededf24579dcf8b6608917516219921db0bfc07f
sharmi-manoj <sharmivrk@gmail.com>
2026-03-31 06:27:35 +0530

    unused files removed

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
-import Header from "./components/Header";
-import Card from "./components/Card";
-import TodoContainer from "./components/TodoContainer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useState } from "react";
-import Login from "./pages/Login"
+import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Landing from "./pages/Landing";
 
-function App()
-{
-     const [users,setUsers] = useState(
-        [
-            {
-                username:"sharmi",
-                password:"123"
-            }
-        ]
-    )
-  return(
+function App() {
+  const [users, setUsers] = useState([
+    {
+      username: "sharmi",
+      password: "123",
+    },
+  ]);
+  return (
     <div>
-    <BrowserRouter>
-
-    
-      <Routes>
-        <Route path="/" element={<Login users={users} setUsers={setUsers}/>}></Route>
-        <Route path="/signup" element={<Signup users={users} setUsers={setUsers} />}></Route>
-        <Route path="/landing" element={<Landing />}></Route>
-       
-      </Routes>
-    </BrowserRouter>
-  </div>
-  )
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<Login users={users} setUsers={setUsers} />}
+          ></Route>
+          <Route
+            path="/signup"
+            element={<Signup users={users} setUsers={setUsers} />}
+          ></Route>
+          <Route path="/landing" element={<Landing />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
